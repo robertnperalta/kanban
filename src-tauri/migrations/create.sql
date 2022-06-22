83c6463ae4bf2526fecbc99edffbd7faf9ec1284
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2022-06-22 08:36:49 -0700

    Add minimally functioning db

---
+++ src-tauri/migrations/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS boards (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS lists (
+    id INTEGER NOT NULL PRIMARY KEY,
+    board_id INTEGER NOT NULL,
+    title VARCHAR(64) NOT NULL,
+    color CHAR(8) NOT NULL,
+    FOREIGN KEY (board_id) REFERENCES Boards(id)
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER NOT NULL PRIMARY KEY,
+    list_id INTEGER NOT NULL,
+    title VARCHAR(256) NOT NULL,
+    description TEXT,
+    time_modified INTEGEREGER,
+    FOREIGN KEY (list_id) REFERENCES Lists(id)
+);
+
+CREATE TABLE IF NOT EXISTS labels (
+    id INTEGER NOT NULL PRIMARY KEY,
+    label VARCHAR(32) NOT NULL,
+    color CHAR(8) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS assignments (
+    task_id INTEGER NOT NULL,
+    label_id INTEGER NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES Tasks(id),
+    FOREIGN KEY (label_id) REFERENCES Labels(id)
+);
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,27 @@
   windows_subsystem = "windows"
 )]
 
+use tauri_plugin_sql::{Migration, MigrationKind, TauriSql};
+
 fn main() {
   tauri::Builder::default()
+    .plugin(TauriSql::default().add_migrations(
+      "sqlite:kanban.db",
+      vec![
+        Migration {
+          version: 1,
+          description: "create db",
+          sql: include_str!("../migrations/create.sql"),
+          kind: MigrationKind::Up,
+        },
+        // Migration {
+        //   version: 1,
+        //   description: "create lsits",
+        //   sql: include_str!("../migrations/lists.sql"),
+        //   kind: MigrationKind::Up,
+        // },
+      ]
+    ))
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
