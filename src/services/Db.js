95cdde81e7de98d254d15713def2856eb351bf98
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2022-06-25 18:51:28 -0700

    Add getFullBoard to Db

--- src/services/Db.js
+++ src/services/Db.js
@@ -21,6 +21,26 @@ class Db {
     let conn = await Database.load("sqlite:kanban.db");
     return new Db(conn);
   }
+
+  async getFullBoard(id) {
+    const res = await this.conn.select(
+      "SELECT boards.id AS board_id, boards.title AS board_title, " +
+      "lists.id AS list_id, lists.position AS list_position, lists.title AS list_title, lists.color AS list_color, " +
+      "tasks.id AS task_id, tasks.title AS task_title, tasks.description AS task_desc, tasks.time_modified AS task_modified, " +
+      "labels.id AS label_id, labels.label AS label_text, labels.color AS label_color " +
+      "FROM boards " +
+      "LEFT JOIN lists ON boards.id = lists.board_id " +
+      "LEFT JOIN tasks ON lists.id = tasks.list_id " +
+      "LEFT JOIN assignments ON tasks.id = assignments.task_id " +
+      "LEFT JOIN labels ON assignments.label_id = labels.id " +
+      "WHERE boards.id = $1",
+      [id]
+    );
+    if (res.length === 0) {
+      throw new Error(`Attempting to get full board ${id}, which does not exist`);
+    }
+    return res;
+  }
 }
 
 export default Db.build();
