042d17a15146aff25c18c69a5eb93d867b2ed72c
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2022-06-25 18:51:57 -0700

    Write more exhaustive tests

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,20 +18,60 @@ export default {
   },
   methods: {
     async testDb() {
-      // let db = await Db.build();
       console.log(this.db);
-      console.log(await this.db.boards.add("Board 1"));
-      console.log(await this.db.boards.add("Board 2"));
-      console.log(await this.db.boards.remove(2));
-      console.log(await this.db.boards.add("Board 3"));
-      console.log(await this.db.boards.all());
-      console.log(await this.db.lists.add(1, 0, "List 1", "0xffffff"));
-      console.log(await this.db.lists.add(1, 1, "List 2", "0xffffff"));
-      console.log(await this.db.lists.add(1, 2, "List 3", "0xffffff"));
-      console.log(await this.db.lists.add(1, 3, "List 4", "0xffffff"));
-      console.log(await this.db.lists.all());
-      console.log(await this.db.lists.remove(2));
-      console.log(await this.db.lists.all());
+      console.log("Set up boards");
+      let b1 = await this.db.boards.add("Board 1");
+      let b2 = await this.db.boards.add("Board 2");
+      let b3 = await this.db.boards.add("Board 3");
+      await this.db.boards.remove(b2);
+      await this.db.boards.view(b3);
+      await this.db.boards.update(b3, "Board 3.5");
+      await this.db.boards.view(b1);
+      console.log("Boards: ", await this.db.boards.all());
+
+      console.log("Set up lists");
+      let l1 = await this.db.lists.add(b1, 0, "List 1", "0xff0000");
+      let l2 = await this.db.lists.add(b1, 1, "List 2", "0xff0000");
+      let l3 = await this.db.lists.add(b1, 2, "List 3", "0xff0000");
+      await this.db.lists.add(b3, 0, "List 4", "0x0000ff");
+      let l5 = await this.db.lists.add(b3, 1, "List 5", "0x0000ff");
+      await this.db.lists.add(b3, 2, "List 6", "0x0000ff");
+      await this.db.lists.remove(l2);
+      await this.db.lists.update(l3, "List 3.5", "0xff0000");
+      console.log("Lists: ", await this.db.lists.all());
+
+      console.log("Set up tasks");
+      let t1 = await this.db.tasks.add(l1, "Task 1", "Something");
+      let t2 = await this.db.tasks.add(l1, "Task 2", "Something else");
+      let t3 = await this.db.tasks.add(l5, "Task 3", null);
+      let t4 = await this.db.tasks.add(l5, "Task 4", null);
+      await this.db.tasks.remove(t4);
+      await this.db.tasks.update(t2, l1, "Task 2.5", "Something else");
+      console.log("Tasks: ", await this.db.tasks.all());
+
+      console.log("Set up labels");
+      let a1 = await this.db.labels.add("Important", "0xff0000");
+      let a2 = await this.db.labels.add("Todo", "0x00ff00");
+      let a3 = await this.db.labels.add("Won't do", "0x000000");
+      let a4 = await this.db.labels.add("Eh", "0x0000ff");
+      await this.db.labels.remove(a3);
+      await this.db.labels.update(a4, "Do Later", "0x0000ff");
+      console.log("Labels :", await this.db.labels.all());
+
+      console.log("Set up label assignments");
+      await this.db.tasks.addLabel(t1, a1);
+      await this.db.tasks.addLabel(t1, a2);
+      await this.db.tasks.addLabel(t3, a1);
+      await this.db.tasks.addLabel(t3, a2);
+      await this.db.tasks.addLabel(t3, a4);
+      await this.db.tasks.removeLabel(t3, a4);
+
+      console.log("Get full board");
+      let fullB1 = await this.db.getFullBoard(b1);
+      console.log(fullB1);
+
+      console.log("Remove board");
+      await this.db.boards.remove(b3);
     }
   }
 }
